33007ac118e80bd537280a9cb380463260ab2d59
EtherK <165754851+Kei07R@users.noreply.github.com>
2024-04-24 01:51:01 +0530

    Update CallToAction.jsx

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,20 +5,22 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Made by Kartikeiya Rai & Riya Rai
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Feel free to connect :D
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
-                </a>
+            <a href="mailto:kartikeiya.rai@mitaoe.ac.in, riya.rai@mitaoe.ac.in" target='_blank' rel='noopener noreferrer'>
+                Connect
+            </a>
+
             </Button>
         </div>
-        <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        <div class="p-7 flex-1">
+            <img src="https://i.pinimg.com/originals/7f/b6/01/7fb60191f817b477baba8fe8c35fc298.jpg" class="mx-auto w-75 h-60" />
         </div>
+
     </div>
   )
 }
